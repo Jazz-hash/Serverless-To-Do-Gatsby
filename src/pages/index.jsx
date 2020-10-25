4ea1fe33c4a5e6878d6571d5b764f9be56283409
Jazz-hash <jazzelmehmood6@gmail.com>
2020-10-25 20:15:13 +0500

    Doneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,23 +16,13 @@ import { makeStyles } from "@material-ui/core/styles"
 import DeleteIcon from "@material-ui/icons/Delete"
 import AddIcon from "@material-ui/icons/Add"
 import { gql, useMutation, useQuery } from "@apollo/client"
+import Todo from "../components/Todo"
 
 const ADD_TODO = gql`
   mutation AddTodo($title: String!) {
     addTodo(title: $title) {
       id
     }
-  }
-`
-
-const DELETE_TODO = gql`
-  mutation deleteTodo($id: String!) {
-    deleteTodo(id: $id)
-  }
-`
-const UPDATE_TODO = gql`
-  mutation updateTodo($id: String!, $done: Boolean!) {
-    updateTodo(id: $id, done: $done)
   }
 `
 
@@ -65,8 +55,6 @@ export default function Home() {
   const [checked, setChecked] = React.useState(false)
   const { data, loading, error, refetch } = useQuery(GET_TODOS)
   const [addTodo] = useMutation(ADD_TODO)
-  const [deleteTodo] = useMutation(DELETE_TODO)
-  const [updateTodo] = useMutation(UPDATE_TODO)
 
   const [todoText, setTodoText] = useState("")
 
@@ -76,26 +64,6 @@ export default function Home() {
     })
     refetch()
   }
-
-  const updateTodoFunc = (id, done) => {
-    updateTodo({
-      variables: { id: id, done: done },
-    })
-    refetch()
-  }
-
-  // const handleToggle = value => () => {
-  //   const currentIndex = checked.indexOf(value)
-  //   const newChecked = [...checked]
-
-  //   if (currentIndex === -1) {
-  //     newChecked.push(value)
-  //   } else {
-  //     newChecked.splice(currentIndex, 1)
-  //   }
-
-  //   setChecked(newChecked)
-  // }
 
   return (
     <Container className={classes.page}>
@@ -123,42 +91,7 @@ export default function Home() {
       {!loading && !error && (
         <List className={classes.root}>
           {data.todos.map(todo => (
-            <ListItem
-              key={todo.id}
-              role={undefined}
-              dense
-              button
-              onClick={() => {
-                console.log("updateTodoDone")
-                updateTodoFunc(todo.id, todo.done)
-                setChecked(!todo.done)
-                console.log("refetching")
-              }}
-            >
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={todo.done}
-                  tabIndex={-1}
-                  disableRipple
-                />
-              </ListItemIcon>
-              <ListItemText id={todo.id} primary={`${todo.title}`} />
-              <ListItemSecondaryAction>
-                <IconButton
-                  edge="end"
-                  aria-label="comments"
-                  onClick={() => {
-                    deleteTodo({
-                      variables: { id: todo.id },
-                    })
-                    refetch()
-                  }}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </ListItemSecondaryAction>
-            </ListItem>
+            <Todo todo={todo} />
           ))}
         </List>
       )}
